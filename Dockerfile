01bec099f4b0dc912842b277ec30af8b80f1121e
anderssonw <wandersson.98@gmail.com>
2024-05-24 14:44:53 +0200

    setup docker and build

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM eclipse-temurin:21-alpine
+RUN apk update && apk upgrade
+
+EXPOSE 8080:8080
+RUN mkdir /app
+WORKDIR .
+COPY build/libs/*.jar /app/matrikkel-egenregistrering-bygning-poc.jar
+ENTRYPOINT ["java","-jar","/app/matrikkel-egenregistrering-bygning-poc.jar"]
--- build.gradle.kts
+++ build.gradle.kts
@@ -28,3 +28,9 @@ dependencies {
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
+
+ktor {
+    fatJar {
+        archiveFileName.set("matrikkel-egenregistrering-bygning-poc.jar")
+    }
+}
