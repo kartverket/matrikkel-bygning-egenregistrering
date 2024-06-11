30b7f93f8e179f3f829674792d2160f9e49461aa
anderssonw <wandersson.98@gmail.com>
2024-06-11 09:21:13 +0200

    add dummy route for checking egenreg connection

--- src/main/kotlin/no/kartverket/matrikkel/bygning/routes/v1/BaseRoutes.kt
+++ src/main/kotlin/no/kartverket/matrikkel/bygning/routes/v1/BaseRoutes.kt
@@ -1,7 +1,9 @@
 package no.kartverket.matrikkel.bygning.routes.v1
 
 import io.bkbn.kompendium.core.routes.swagger
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import no.kartverket.matrikkel.bygning.services.EgenregistreringsService
 
@@ -10,6 +12,13 @@ fun Application.baseRoutesV1(
 ) {
     routing {
         swagger()
+
+        // TODO Remove after checking connection between Egenreg and Bygning
+        route("/dummy") {
+            get {
+                call.respondText("Hei Egenregistrering!", status = HttpStatusCode.OK)
+            }
+        }
 
         route("/v1") {
             egenregistreringRouting(egenregistreringsService)
