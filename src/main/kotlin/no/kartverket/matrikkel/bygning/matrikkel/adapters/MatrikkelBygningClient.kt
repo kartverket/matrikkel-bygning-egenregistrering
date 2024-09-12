4730657c99b869aec65fbf498afa680ab85da128
Tor Egil Strand <strant@kartverket.no>
2024-09-12 09:34:39 +0200

    TH-58: Mer typesikker bruk av StoreService

--- src/main/kotlin/no/kartverket/matrikkel/bygning/matrikkel/adapters/MatrikkelBygningClient.kt
+++ src/main/kotlin/no/kartverket/matrikkel/bygning/matrikkel/adapters/MatrikkelBygningClient.kt
@@ -2,20 +2,18 @@ package no.kartverket.matrikkel.bygning.matrikkel.adapters
 
 import no.kartverket.matrikkel.bygning.matrikkel.BygningClient
 import no.kartverket.matrikkel.bygning.matrikkelapi.MatrikkelApi
-import no.kartverket.matrikkel.bygning.matrikkelapi.getObjectAs
-import no.kartverket.matrikkel.bygning.matrikkelapi.getObjectsAs
+import no.kartverket.matrikkel.bygning.matrikkelapi.getBruksenheter
+import no.kartverket.matrikkel.bygning.matrikkelapi.getBygning
 import no.kartverket.matrikkel.bygning.models.Bruksenhet
 import no.kartverket.matrikkel.bygning.models.Bygning
 import no.statkart.matrikkel.matrikkelapi.wsapi.v1.domain.bygning.BygningId
 import no.statkart.matrikkel.matrikkelapi.wsapi.v1.service.store.ServiceException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import no.statkart.matrikkel.matrikkelapi.wsapi.v1.domain.bygning.Bruksenhet as MatrikkelBruksenhet
-import no.statkart.matrikkel.matrikkelapi.wsapi.v1.domain.bygning.Bygning as MatrikkelBygning
 
 // TODO Håndtering av at matrikkel servicene thrower på visse vanlige HTTP koder, ikke bare full try/catch
 internal class MatrikkelBygningClient(
-    val matrikkelApi: MatrikkelApi.WithAuth
+    private val matrikkelApi: MatrikkelApi.WithAuth
 ) : BygningClient {
     private val log: Logger = LoggerFactory.getLogger(javaClass)
 
@@ -23,10 +21,9 @@ internal class MatrikkelBygningClient(
         val bygningId: BygningId = BygningId().apply { value = id }
 
         try {
-            val bygning = matrikkelApi.storeService().getObjectAs<MatrikkelBygning>(bygningId, matrikkelApi.matrikkelContext)
+            val bygning = matrikkelApi.storeService().getBygning(bygningId, matrikkelApi.matrikkelContext)
 
-            val bruksenheter =
-                matrikkelApi.storeService().getObjectsAs<MatrikkelBruksenhet>(bygning.bruksenhetIds.item, matrikkelApi.matrikkelContext)
+            val bruksenheter = matrikkelApi.storeService().getBruksenheter(bygning.bruksenhetIds.item, matrikkelApi.matrikkelContext)
 
             return Bygning(
                 bygningId = bygning.id.value,
