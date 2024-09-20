0ab9023ae4ea36176c57d868ed877c118cc5407f
Tor Egil Strand <strant@kartverket.no>
2024-09-20 08:54:46 +0200

    TH-58: Forenkler funksjonsdefinisjon

--- matrikkel-api/src/testFixtures/kotlin/no/kartverket/matrikkel/bygning/matrikkelapi/builders/DatoBuilders.kt
+++ matrikkel-api/src/testFixtures/kotlin/no/kartverket/matrikkel/bygning/matrikkelapi/builders/DatoBuilders.kt
@@ -5,8 +5,6 @@ import javax.xml.datatype.DatatypeFactory
 
 private val datatypeFactory by lazy { DatatypeFactory.newDefaultInstance() }
 
-fun timestampUtc(year: Int, month: Int, day: Int, hour: Int = 0, minute: Int = 0, second: Int = 0): Timestamp {
-    val timestamp = Timestamp()
-    timestamp.timestamp = datatypeFactory.newXMLGregorianCalendar(year, month, day, hour, minute, second, 0, 0)
-    return timestamp
+fun timestampUtc(year: Int, month: Int, day: Int, hour: Int = 0, minute: Int = 0, second: Int = 0) = Timestamp().apply {
+    this.timestamp = datatypeFactory.newXMLGregorianCalendar(year, month, day, hour, minute, second, 0, 0)
 }
