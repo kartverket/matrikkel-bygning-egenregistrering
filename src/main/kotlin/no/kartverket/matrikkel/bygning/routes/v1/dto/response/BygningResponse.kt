910d8888f3aebf1e172fd98287d59fc92f0cfa8e
William Andersson <wandersson.98@gmail.com>
2024-09-24 13:52:44 +0200

    Gjør Multikilderesponse nullable (#95)
    * add nullability for multikilderesponses
    
    * set explicit null for client content negotiation and add test for null check
    
    * revert debugging extraction

--- src/main/kotlin/no/kartverket/matrikkel/bygning/routes/v1/dto/response/BygningResponse.kt
+++ src/main/kotlin/no/kartverket/matrikkel/bygning/routes/v1/dto/response/BygningResponse.kt
@@ -19,20 +19,31 @@ import no.kartverket.matrikkel.bygning.serializers.InstantSerializer
 import java.time.Instant
 
 @Serializable
+data class MultikildeResponse<T : Any>(
+    val autoritativ: T? = null,
+    val egenregistrert: T? = null
+)
+
+@Serializable
 data class BygningResponse(
     val bygningId: Long,
     val bygningsnummer: Long,
-    val byggeaar: MultikildeResponse<ByggeaarResponse>,
-    val bruksareal: MultikildeResponse<BruksarealResponse>,
-    val vannforsyning: MultikildeResponse<VannforsyningKodeResponse>,
-    val avlop: MultikildeResponse<AvlopKodeResponse>,
-    val energikilder: MultikildeResponse<List<EnergikildeResponse>>,
-    val oppvarming: MultikildeResponse<List<OppvarmingResponse>>,
+    val byggeaar: MultikildeResponse<ByggeaarResponse>?,
+    val bruksareal: MultikildeResponse<BruksarealResponse>?,
+    val vannforsyning: MultikildeResponse<VannforsyningKodeResponse>?,
+    val avlop: MultikildeResponse<AvlopKodeResponse>?,
+    val energikilder: MultikildeResponse<List<EnergikildeResponse>>?,
+    val oppvarming: MultikildeResponse<List<OppvarmingResponse>>?,
     val bruksenheter: List<BruksenhetResponse>,
 )
 
 @Serializable
-data class MultikildeResponse<T : Any>(val autoritativ: T? = null, val egenregistrert: T? = null)
+data class BruksenhetResponse(
+    val bruksenhetId: Long,
+    val bruksareal: MultikildeResponse<BruksarealResponse>?,
+    val energikilder: MultikildeResponse<List<EnergikildeResponse>>?,
+    val oppvarminger: MultikildeResponse<List<OppvarmingResponse>>?,
+)
 
 @Serializable
 data class RegisterMetadataResponse(@Serializable(with = InstantSerializer::class) val registreringstidspunkt: Instant)
@@ -55,23 +66,16 @@ data class EnergikildeResponse(val data: EnergikildeKode?, val metadata: Registe
 @Serializable
 data class OppvarmingResponse(val data: OppvarmingKode?, val metadata: RegisterMetadataResponse)
 
-@Serializable
-data class BruksenhetResponse(
-    val bruksenhetId: Long,
-    val bruksareal: MultikildeResponse<BruksarealResponse>,
-    val energikilder: MultikildeResponse<List<EnergikildeResponse>>,
-    val oppvarminger: MultikildeResponse<List<OppvarmingResponse>>,
-)
 
-fun RegisterMetadata.toRegistermetadataResponse() = RegisterMetadataResponse(
+fun RegisterMetadata.toRegisterMetadataResponse() = RegisterMetadataResponse(
     registreringstidspunkt = this.registreringstidspunkt,
 )
 
-fun <T : Any, R : Any> Multikilde<T>.toMultikildeResponse(mapper: T.() -> R): MultikildeResponse<R> {
+fun <T : Any, R : Any> Multikilde<T>.toMultikildeResponse(mapper: T.() -> R): MultikildeResponse<R>? {
     return MultikildeResponse(
         autoritativ?.mapper(),
         egenregistrert?.mapper(),
-    )
+    ).takeUnless { autoritativ == null && egenregistrert == null }
 }
 
 fun Bygning.toBygningResponse(): BygningResponse = BygningResponse(
@@ -86,26 +90,6 @@ fun Bygning.toBygningResponse(): BygningResponse = BygningResponse(
     oppvarming = this.oppvarminger.toMultikildeResponse { map(Oppvarming::toOppvarmingResponse) },
 )
 
-private fun Byggeaar.toByggeaarResponse(): ByggeaarResponse = ByggeaarResponse(
-    data = this.data,
-    metadata = metadata.toRegistermetadataResponse(),
-)
-
-private fun Bruksareal.toBruksarealResponse(): BruksarealResponse = BruksarealResponse(
-    data = this.data,
-    metadata = metadata.toRegistermetadataResponse(),
-)
-
-private fun Vannforsyning.toVannforsyningResponse(): VannforsyningKodeResponse = VannforsyningKodeResponse(
-    data = this.data,
-    metadata = metadata.toRegistermetadataResponse(),
-)
-
-private fun Avlop.toAvlopKodeResponse(): AvlopKodeResponse = AvlopKodeResponse(
-    data = this.data,
-    metadata = metadata.toRegistermetadataResponse(),
-)
-
 fun Bruksenhet.toBruksenhetResponse(): BruksenhetResponse = BruksenhetResponse(
     bruksenhetId = this.bruksenhetId,
     bruksareal = this.bruksareal.toMultikildeResponse(Bruksareal::toBruksarealResponse),
@@ -113,12 +97,32 @@ fun Bruksenhet.toBruksenhetResponse(): BruksenhetResponse = BruksenhetResponse(
     oppvarminger = this.oppvarminger.toMultikildeResponse { map(Oppvarming::toOppvarmingResponse) },
 )
 
+private fun Byggeaar.toByggeaarResponse(): ByggeaarResponse = ByggeaarResponse(
+    data = this.data,
+    metadata = metadata.toRegisterMetadataResponse(),
+)
+
+private fun Bruksareal.toBruksarealResponse(): BruksarealResponse = BruksarealResponse(
+    data = this.data,
+    metadata = metadata.toRegisterMetadataResponse(),
+)
+
+private fun Vannforsyning.toVannforsyningResponse(): VannforsyningKodeResponse = VannforsyningKodeResponse(
+    data = this.data,
+    metadata = metadata.toRegisterMetadataResponse(),
+)
+
+private fun Avlop.toAvlopKodeResponse(): AvlopKodeResponse = AvlopKodeResponse(
+    data = this.data,
+    metadata = metadata.toRegisterMetadataResponse(),
+)
+
 private fun Energikilde.toEnergikildeResponse() = EnergikildeResponse(
     data = this.data,
-    metadata = metadata.toRegistermetadataResponse(),
+    metadata = metadata.toRegisterMetadataResponse(),
 )
 
 private fun Oppvarming.toOppvarmingResponse() = OppvarmingResponse(
     data = this.data,
-    metadata = metadata.toRegistermetadataResponse(),
+    metadata = metadata.toRegisterMetadataResponse(),
 )
