959a07e28cb6dc11efe57eb2db9b1e93323ea672
William Andersson <wandersson.98@gmail.com>
2024-11-05 10:00:54 +0100

    Filtrering av åpenbare feilkilder i byggeårsutledning, dokumentasjon for regler (#131)
    Legger til ny regel for utledning av byggeår, hvor vedtaksdatoen nå ikke kan være satt til å være etter registreringsdato. Dette er gjort av to grunner:
    1. Det er ikke mulig å gjøre i dagens matrikkel, som tilsier at det kun skal ha skjedd der hvor registeringsdato og vedtaksdato har blitt byttet ved uhell (for eksempel under manuell innsending)
    2. Det skal ikke være mulig å føre vedtaksdato frem i tid
    
    JIRA: TB-96

---
+++ docs/byggeaar-utledning.md
@@ -0,0 +1,23 @@
+# Utledning av byggeår
+
+## Bakgrunn
+
+Per 31.10.2024 har ikke matrikkelen byggeår satt for verken bygninger eller bruksenheter. Det er flere andre
+virksomheter i Norge som har både forsøkt å utlede byggeår fra eksisterende data, eller satt dette selv ut i fra
+dokumentasjon/egenregistrering de selv innhenter. Dette gjelder blant annet Enova og SSB. Det har vært uttrykt ønske om
+at byggeår settes som et nytt felt for bygninger, og kanskje bruksenheter, i selve eiendomsregistret, slik at man har én
+kilde til sannhet for dette.
+
+## Utledning for bygninger
+
+Etter en del graving har det blitt vurdert at byggeår kan utledes ved å se på de historiske bygningsstatusene til en
+bygning. For at man skal kunne utlede byggeåret til bygningen må et sett med regler for bygningsstatusene bygningen har
+gjelde:
+
+1. Vi vurderer kun bygningsstatuskode FerdigAttest og MidlertidigBrukstillatelse
+2. Statusen må være registrert fra og med 25.04.2009*
+3. Statusen skal være gjeldende, og ikke være en status markert som slettet
+4. Vedtaksdato skal ikke være etter registreringsdato
+
+\* _Datoen er satt i forbindelse med at alle kommuner var over på et system hvor de kunne føre ferdigattest og
+midlertidig brukstillatelse_  
--- infrastructure/src/main/kotlin/no/kartverket/matrikkel/bygning/infrastructure/matrikkel/client/ByggeaarDeriver.kt
+++ infrastructure/src/main/kotlin/no/kartverket/matrikkel/bygning/infrastructure/matrikkel/client/ByggeaarDeriver.kt
@@ -7,16 +7,14 @@ import no.statkart.matrikkel.matrikkelapi.wsapi.v1.domain.bygning.koder.Bygnings
 import java.time.LocalDate
 import no.statkart.matrikkel.matrikkelapi.wsapi.v1.domain.bygning.Bygning as MatrikkelBygning
 
-// TODO: Hvordan dokumentere hvor denne datoen kommer fra?
 private val EARLIEST_DATE_FOR_DERIVING_BYGGEAAR = LocalDate.of(2009, 4, 25)
 
-// TODO: Det vil være mulig å få byggeår hvor bygningsstatus er veldig langt tilbakedatert, og vi kan nok anta at disse er feil.
-// Må gå opp hva "smerteterskelen" for hva som regnes som en ikke-godkjent bygningsstatusdato er
 internal fun deriveByggeaarForBygning(bygning: MatrikkelBygning): Int? {
     return bygning.bygningsstatusHistorikker.item
         .filter { isAfterThresholdDate(it) }
         .filter { isCorrectBygningsstatusKode(it.bygningsstatusKodeId) }
         .filter { isNotDeleted(it) }
+        .filter { isRegistrertDatoAfterVedtaksdato(it) }
         .minByOrNull { it.dato.toLocalDate() }
         ?.dato
         ?.toLocalDate()
@@ -30,3 +28,5 @@ private fun isCorrectBygningsstatusKode(bygningsstatusKodeId: BygningsstatusKode
     bygningsstatusKodeId.value == MatrikkelBygningsstatusKode.FerdigAttest().value || bygningsstatusKodeId.value == MatrikkelBygningsstatusKode.MidlertidigBrukstillatelse().value
 
 private fun isNotDeleted(bygningsstatus: BygningsstatusHistorikk): Boolean = bygningsstatus.slettetDato == null
+
+private fun isRegistrertDatoAfterVedtaksdato(bygningsstatus: BygningsstatusHistorikk): Boolean = bygningsstatus.dato.toLocalDate() <= bygningsstatus.registrertDato.toLocalDate()
