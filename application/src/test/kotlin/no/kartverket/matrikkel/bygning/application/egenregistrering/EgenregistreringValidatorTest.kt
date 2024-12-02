5f8de4e99185f87dc9dcdfc79796763025f12f6f
espgus <gustav.espegren@kartverket.no>
2024-12-02 12:00:25 +0100

    Validering av kildemateriale
    Laget enkel logikk på hvilke kildemateriale som kan registeres på hvilken egenregisteringer

--- application/src/test/kotlin/no/kartverket/matrikkel/bygning/application/egenregistrering/EgenregistreringValidatorTest.kt
+++ application/src/test/kotlin/no/kartverket/matrikkel/bygning/application/egenregistrering/EgenregistreringValidatorTest.kt
@@ -1,21 +1,29 @@
 package no.kartverket.matrikkel.bygning.application.egenregistrering
 
 import assertk.assertThat
+import assertk.assertions.any
 import assertk.assertions.contains
 import assertk.assertions.hasSize
+import assertk.assertions.isFalse
 import assertk.assertions.isTrue
+import no.kartverket.matrikkel.bygning.application.models.AvlopRegistrering
 import no.kartverket.matrikkel.bygning.application.models.BruksarealRegistrering
 import no.kartverket.matrikkel.bygning.application.models.Bruksenhet
 import no.kartverket.matrikkel.bygning.application.models.BruksenhetRegistrering
+import no.kartverket.matrikkel.bygning.application.models.ByggeaarRegistrering
 import no.kartverket.matrikkel.bygning.application.models.Bygning
 import no.kartverket.matrikkel.bygning.application.models.BygningRegistrering
 import no.kartverket.matrikkel.bygning.application.models.Egenregistrering
+import no.kartverket.matrikkel.bygning.application.models.EnergikildeRegistrering
 import no.kartverket.matrikkel.bygning.application.models.EtasjeBruksarealRegistrering
 import no.kartverket.matrikkel.bygning.application.models.Etasjebetegnelse
 import no.kartverket.matrikkel.bygning.application.models.Etasjenummer
 import no.kartverket.matrikkel.bygning.application.models.Multikilde
 import no.kartverket.matrikkel.bygning.application.models.RegistreringAktoer.Foedselsnummer
+import no.kartverket.matrikkel.bygning.application.models.kodelister.AvlopKode
+import no.kartverket.matrikkel.bygning.application.models.kodelister.EnergikildeKode
 import no.kartverket.matrikkel.bygning.application.models.kodelister.EtasjeplanKode
+import no.kartverket.matrikkel.bygning.application.models.kodelister.KildematerialeKode
 import java.time.Instant
 import java.util.UUID
 import kotlin.test.Test
@@ -40,6 +48,100 @@ class EgenregistreringValidatorTest {
             bruksenhetRegistreringer = emptyList(),
         ),
     )
+
+    @Test
+    fun `egenregistrering med ugyldig kildemateriale for byggeaar skal feile`() {
+        val validationResult = EgenregistreringValidator.validateEgenregistrering(
+            baseEgenregistrering.copy(
+                bygningRegistrering = baseEgenregistrering.bygningRegistrering.copy(
+                    bruksenhetRegistreringer = listOf(
+                            BruksenhetRegistrering(
+                                bruksenhetId = 1L,
+                                byggeaarRegistrering = ByggeaarRegistrering(
+                                    1990,
+                                    KildematerialeKode.Plantegninger
+                                ),
+                                bruksarealRegistrering = null,
+                                energikildeRegistrering = null,
+                                oppvarmingRegistrering = null,
+                                vannforsyningRegistrering = null,
+                                avlopRegistrering = null,
+                            )
+                    )
+                )
+            ),
+            baseBygning
+        )
+
+        assertThat(validationResult.isErr).isTrue()
+        assertThat(validationResult.error.errors).hasSize(1)
+        assertThat(validationResult.error.errors.first().message).contains("ID: 1")
+        assertThat(validationResult.error.errors.first().message).contains("kildemateriale: Plantegninger")
+    }
+
+    @Test
+    fun `egenregistrering med gyldig kildemateriale for energikilde skal passere`() {
+        val validationResult = EgenregistreringValidator.validateEgenregistrering(
+            baseEgenregistrering.copy(
+                bygningRegistrering = baseEgenregistrering.bygningRegistrering.copy(
+                    bruksenhetRegistreringer = listOf(
+                        BruksenhetRegistrering(
+                            bruksenhetId = 1L,
+                            bruksarealRegistrering = null,
+                            oppvarmingRegistrering = null,
+                            byggeaarRegistrering = null,
+                            vannforsyningRegistrering = null,
+                            energikildeRegistrering = EnergikildeRegistrering(
+                                listOf( EnergikildeKode.AnnenEnergikilde),
+                                KildematerialeKode.Selvrapportert
+                            ),
+                            avlopRegistrering = null
+                        )
+                    )
+                )
+            ),
+            baseBygning
+        )
+
+        assertThat(validationResult.isErr).isFalse()
+    }
+
+    @Test
+    fun `egenregistrering med flere ugyldige kildematerialer skal liste alle feilene`() {
+        val validationResult = EgenregistreringValidator.validateEgenregistrering(
+            baseEgenregistrering.copy(
+                bygningRegistrering = baseEgenregistrering.bygningRegistrering.copy(
+                    bruksenhetRegistreringer = listOf(
+                        BruksenhetRegistrering(
+                            bruksenhetId = 1L,
+                            bruksarealRegistrering = null,
+                            energikildeRegistrering = null,
+                            oppvarmingRegistrering = null,
+                            vannforsyningRegistrering = null,
+                            byggeaarRegistrering = ByggeaarRegistrering(
+                                1990,
+                                KildematerialeKode.Plantegninger
+                            ),
+                            avlopRegistrering = AvlopRegistrering(
+                                AvlopKode.OffentligKloakk,
+                                KildematerialeKode.Plantegninger
+                            )
+                        )
+                    )
+                )
+            ),
+            baseBygning
+        )
+
+        assertThat(validationResult.isErr).isTrue()
+        assertThat(validationResult.error.errors).hasSize(2)
+        val errorMessages = validationResult.error.errors.map { it.message }
+        assertThat(errorMessages).any { it.contains("ID: 1") }
+        assertThat(errorMessages).any { it.contains("AvlopRegistrering") }
+        assertThat(errorMessages).any { it.contains("ByggeaarRegistrering") }
+        assertThat(errorMessages).any { it.contains("kildemateriale: Plantegninger") }
+    }
+
 
     @Test
     fun `egenregistrering med flere registreringer paa samme bruksenhet skal feile`() {
