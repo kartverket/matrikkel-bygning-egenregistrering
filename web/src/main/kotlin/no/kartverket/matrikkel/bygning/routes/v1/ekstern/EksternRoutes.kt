3c609794d3ddf3e1ccf97fff423a12b072b2662a
espgus <gustav.espegren@kartverket.no>
2025-01-13 09:47:22 +0100

    Skiller ut eksern endepunkt i eget spec
    * eksterne endepunkter er skilt ut i egen spec. Gjøres for at eksterne brukere skal kunne ta i bruk relevante løsninger uten internt støy.
    
    - TB-132

--- web/src/main/kotlin/no/kartverket/matrikkel/bygning/routes/v1/ekstern/EksternRoutes.kt
+++ web/src/main/kotlin/no/kartverket/matrikkel/bygning/routes/v1/ekstern/EksternRoutes.kt
@@ -14,6 +14,7 @@ fun Route.eksternRouting(
         route(
             "/ekstern",
             {
+                specId = "ekstern"
                 response {
                     code(HttpStatusCode.Unauthorized) {
                         description = "Manglende eller ugyldig token"
