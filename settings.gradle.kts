eb15518c13947ca58ee269b55f4beb889b33a09f
Ida Gylvik <ida.gylvik@kartverket.no>
2025-01-15 13:07:48 +0100

    Ta i bruk felles matrikkel wsclient
    Slutte å generere Java-typer basert på Matrikkel API i hvert repo, og i stede ta i bruk felles matrikkel wsclient som gjør det for oss. Legger til wsclient og sletter gamle byggesteg og genererte typer. Legge til bruk av et Github PAT for tilgang til pakken via Github Packages, lokalt og i workflows.
    
    TS-130

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,10 +6,23 @@ rootProject.name = "matrikkel-bygning-egenregistrering"
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
+        maven {
+            name = "WSClient"
+            url = uri("https://maven.pkg.github.com/kartverket/matrikkelapi-v1-wsclient")
+
+            val token = System.getenv("KV_PACKAGES_PAT") // For GitHub workflows
+                ?: extra["KV_PACKAGES_PAT"] // For local development
+                ?: throw InvalidUserDataException("missing 'KV_PACKAGES_PAT' property")
+            val tokenString = token as String
+
+            credentials {
+                username = "x-access-token" // Dummy username
+                password = tokenString
+            }
+        }
     }
 }
 
-include(":matrikkel-api")
 include(":application")
 include(":infrastructure")
 include(":web")
