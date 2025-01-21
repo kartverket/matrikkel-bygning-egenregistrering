af85de2cf53af1854161b4d1749693f147586e7b
anderssonw <wandersson.98@gmail.com>
2025-01-21 09:44:16 +0100

    fjern ubrukte parametre for mock jwts

--- web/src/integrationTest/kotlin/no/kartverket/matrikkel/bygning/v1/common/MockOAuth2ServerExtensions.kt
+++ web/src/integrationTest/kotlin/no/kartverket/matrikkel/bygning/v1/common/MockOAuth2ServerExtensions.kt
@@ -4,13 +4,11 @@ import no.nav.security.mock.oauth2.MockOAuth2Server
 
 class MockOAuth2ServerExtensions() {
     companion object {
-        private const val DEFAULT_SUBJECT = "123456789"
         private const val DEFAULT_ISSUER = "testIssuer"
         private const val DEFAULT_ORG = "123456789"
 
         fun MockOAuth2Server.issueMaskinportenJWT(scope: String = "kartverk:riktig:scope") = issueToken(
             issuerId = DEFAULT_ISSUER,
-            subject = DEFAULT_SUBJECT,
             claims = mapOf(
                 "orgno" to DEFAULT_ORG,
                 "scope" to scope,
@@ -19,9 +17,7 @@ class MockOAuth2ServerExtensions() {
 
         fun MockOAuth2Server.issueIDPortenJWT() = issueToken(
             issuerId = DEFAULT_ISSUER,
-            subject = DEFAULT_SUBJECT,
             claims = mapOf(
-                "orgno" to DEFAULT_ORG,
                 "pid" to "31129956715",
             ),
         )
